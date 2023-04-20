a4573e33b5ec580287917d998813262dacbe21b2
WilhelmWidlund <erik.widlundmellergard@epfl.ch>
2023-04-20 13:26:47 +0200

    Added the EagerMOT framework

---
+++ EagerMOT/configs/local_variables.py
@@ -0,0 +1,9 @@
+# Chooose MOUNT_PATH to be the place where the repo folder is
+MOUNT_PATH = "C:\Users\wilhe\Documents\DLAV"  # in case you are mounting data storage externally
+SPLIT = 'mini_val'
+
+KITTI_WORK_DIR = MOUNT_PATH + "/storage/slurm/kimal/eagermot_workspace/kitti"
+KITTI_DATA_DIR = MOUNT_PATH + "/storage/slurm/osep/datasets/kitti"
+
+NUSCENES_WORK_DIR = MOUNT_PATH + "\DLAV_GuillaumeMOT\Workspaces\NuScenes"
+NUSCENES_DATA_DIR = MOUNT_PATH + "\DLAV_GuillaumeMOT\Datasets\NuScenes"
